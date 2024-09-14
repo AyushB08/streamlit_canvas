649b4cfa9112ba56a6dfb0857814ea7cef487b1b
Jainish Patel <jaicode2020@gmail.com>
2024-09-14 02:10:16 -0400

    print

--- app.py
+++ app.py
@@ -221,6 +221,9 @@ def submit_mask_data(base_url, reference_url, email):
             st.error("Backend URL is not set. Please check your environment variables.")
             return
 
+        print(f'base: {base64_base}')
+        print(f'reference: {base64_ref}')
+
         try:
             response = requests.post(
                 backend_url,
